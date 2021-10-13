54f6023d91e237d2e29574a30c2029188db12637
Fael123Programming <rafaelfonseca1020@gmail.com>
2021-10-13 15:50:48 -0300

    New Commit

--- search/__main__.py
+++ search/__main__.py
@@ -1,46 +1,52 @@
 from binarySearch import binarySearch
 from sequentialSearch import sequentialSearch
-from insertionSort import *
+from insertionSort import insertionSort
 from random import randint
 
-listData = list()
-for counter in range(1000000):  # Generating a million of random numbers.
-    listData.append(randint(0, 1000000))
-insertionSort(listData)  # All values are sorted in an increasing way.
-print("-- SEARCH VALUES --".center(100, "-"))
-opt = int(input("1 - See all generated values\n2 - Search for a number\n3 - Exit\n-> "))
 while True:
-    while not opt in [1, 2, 3]:
-        print("Invalid command")
-        print("-- SEARCH VALUES --".center(100, "-"))
-        opt = int(input("1 - See all generated values\n2 - Search for a number\n3 - Exit\n-> "))
-    if opt == 1:
+    print("-- SEARCH VALUES --".center(100, "-"))
+    quantity = input("How many numbers to generate randomly? ")
+    if not quantity.isnumeric():
+        print("Invalid input")
+    else:
+        break
+listData = list()
+for counter in range(int(quantity)):  # Generating 'quantity' random numbers.
+    listData.append(randint(0, 1000000))  # 0 - 1000000 is the applied range.
+insertionSort(listData)  # All values are sorted in an increasing way.
+while True:
+    print("-- SEARCH VALUES --".center(100, "-"))
+    opt = input("1 - See all generated values\n2 - Search for a number\n3 - Exit\n-> ")
+    if opt == "1":
         print("".center(150, "-"))
-        print(listData)
+        c = 0
+        for data in listData:
+            print(data, end=" ")
+            c += 1
+            if c == 50:
+                print()
+                c = 0
         print("".center(150, "-"))
-    elif opt == 2:
-        num = int(input("Number in range (0 - 1000000): "))
-        while num < 0:
-            print(f"{num} is out of range.")
-            num = int(input("Number in ranger (0 - 1000000): "))
-        srch = int(input("1 - binary search\n2 - sequential search\n-> "))
-        while not srch in [1, 2]:
+    elif opt == "2":
+        while True:
+            num = input("Number in range (0 - 1000000): ")
+            if not num.isnumeric():
+                print("Invalid input")
+            else:
+                break
+        srch = input("1 - binary search\n2 - sequential search\n-> ")
+        while srch != "1" and srch != "2":
             print("Invalid command")
-            srch = int(input("1 - binary search\n2 - sequential search\n-> "))
-        if srch == 1:
-            position = binarySearch(listData, num)
+            srch = input("1 - binary search\n2 - sequential search\n-> ")
+        if srch == "1":
+            position = binarySearch(listData, int(num))
         else:
-            position = sequentialSearch(listData, num)
+            position = sequentialSearch(listData, int(num))
         if position == -1:
             print(f"{num} was not found.")
         else:
             print(f"{num} was found in position {position}.")
-    elif opt == 3:
+    elif opt == "3":
         exit(0)
     else:
         print("Invalid command")
-    print("-- SEARCH VALUES --".center(100, "-"))
-    opt = int(input("1 - See all generated values\n2 - Search for a number\n3 - Exit\n-> "))
-
-
-
--- search/insertionSort.py
+++ search/insertionSort.py
@@ -2,7 +2,7 @@ def insertionSort(listData: list):
     for j in range(1, len(listData)):
         value = listData[j]
         i = j - 1
-        while i + 1 > 0 and listData[i] > value:  # a previous is greater than a next?
+        while i >= 0 and listData[i] > value:  # a previous is greater than a next?
             listData[i + 1] = listData[i]  # shift them!
             i -= 1
         listData[i + 1] = value
