c3097e6b16d88980689e77def551aeecae6ea4ae
Fael123Programming <rafaelfonseca1020@gmail.com>
2021-10-13 21:02:24 -0300

    Updates

--- search/insertionSort.py
+++ search/insertionSort.py
@@ -1,8 +1,17 @@
-def insertionSort(listData: list):
-    for j in range(1, len(listData)):
-        value = listData[j]
-        i = j - 1
-        while i >= 0 and listData[i] > value:  # a previous is greater than a next?
-            listData[i + 1] = listData[i]  # shift them!
-            i -= 1
-        listData[i + 1] = value
+def insertionSort(listData: list, increasingOrder=True):
+    if increasingOrder:  # Ordering in an increasing order.
+        for j in range(1, len(listData)):
+            value = listData[j]
+            i = j - 1
+            while i >= 0 and listData[i] > value:  # a previous is greater than a next?
+                listData[i + 1] = listData[i]  # shift them!
+                i -= 1
+            listData[i + 1] = value
+    else:  # Ordering in a decreasing order.
+        for j in range(1, len(listData)):
+            value = listData[j]
+            i = j - 1
+            while i >= 0 and listData[i] < value:
+                listData[i + 1] = listData[i]
+                i -= 1
+            listData[i + 1] = value
